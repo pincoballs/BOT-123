01ddd7a682da73daec5cf413221d76743162cb98
pincoballs <56087954+pincoballs@users.noreply.github.com>
2019-10-09 22:07:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.on('message', message =>{
 });
 bot.on('message', message =>{
     if(message.content.startsWith(PREFIX + "mao")){
-        number = 3;
+        number = 2;
         var random = Math.floor (Math.random() * (number));
         const embed = new Discord.RichEmbed()
                     .setImage(array.mao[random])
