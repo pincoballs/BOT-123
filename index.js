91bc95434616d2eac79605b97c965fdded6fb16a
Pinco <puffored@gmail.com>
2019-12-09 18:48:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const PREFIX ='?';
-
+const ytdl = require('ytdl-core');
 var fs = require('fs');
 var commandlist = fs.readFileSync('commandlist.txt', 'utf8');
 var comandi = fs.readFileSync('comandi.txt', 'utf8');
@@ -9,20 +9,38 @@ var array=[];
 array = require("./meme.json");
 /*var meme = fs.readFileSync('meme.txt', 'utf8');*/
 function emoji (id) { return clientInformation.emojis.get(id).toString (); }
-const ytdl = require('ytdl-core');
-bot.run = async (client,message,args,ops) =>{
-    if(!message.member.voiceChannel) return message.channel.send('Please connect to a Voicechannel... u idiot');
-    if(message.guild.me.voiceChannel) return message.channel.send('Sorry the bot is already connected in a voice channel');
-    if(!args[0])return message.channel.send('Sorry,please input a url following the command.');
-    message.guild.me.channel.leave();
-    message.channel.send('Adios');
-    let validate = await ytdl.validateURL(args[0]);
-    if(!validate) return message.channel.send('Sorry,please input a Valid URL');
-    let info = await ytdl.getInfo(args[0]);
-    let connection = await message.member.voiceChannel.join();
-    let dispatcher = await connection.play(ytdl(args[0],{filter: 'audioonly'}));
-    message.channel.send(`Now playing : ${info.title}`)
+bot.on('message', async msg => {
+    if(msg.author.bot) return undefined;
+    if(!msg.content.startsWith(PREFIX))return undefined;
+    const args = msg.content.split(' ');
+
+if(msg.content.startsWith(`${PREFIX}play`)||`${PREFIX}p`){
+    const voiceChannel = msg.member.voiceChannel;
+    if(!voiceChannel) return msg.channel.send('You must be in a voice channel to play music... u moron');
+    const permissions = voiceChannel.permissionsFor(msg.client.user);
+
+    const premissions = voiceChannel.permissionsFor(msg.client.user);
+    if(!permissions.has('CONNECT')){
+        return msg.channel.send('I cannot connecto to your voiche channel');
+
+    }
+    if(!permissoions.has('SPEAK')){
+        return msg.channel.send('I cannot speak in this voicechannel');
+    }
+    try {
+        var connection = await voiceChannel.join();
+    }catch(error){
+        console.error(`i cant join the voice channel :${error}`)
+    return msg.channel.send(`i cant join the voice channel :${error}`);
+    }
+    const dispatcher = connection.playStream(ytdl(args[1]));
+    dispatcher.setVolumeLogarithmic(5 / 5);
+}else if (msg.content.startsWith(`${PREFIX}stop`)){
+    if(!msg.member.voiceChannel)return msg.channel.send('You are not in a voice channel ');
+    msg.member.voiceChannel.leave();
+    return undefined;
 }
+})
 bot.on('ready',() => { console.log("I'm ready to send nudes!"); } )
 
 bot.on('guildMemberAdd', member => {
